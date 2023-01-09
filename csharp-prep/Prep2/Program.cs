f24b107e5fb4cc51fe360aad09fe4726ce36011e
Jammonjones <wjssaan@gmail.com>
2023-01-08 17:11:32 -0700

    prep 1 and 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        string letter = "";
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        float percentagenum = Single.Parse(percentage);
+        if (percentagenum >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentagenum >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentagenum >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentagenum >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is: {letter}");
+    }   
 }
