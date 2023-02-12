992a0011b3817484d6fb24cad9c4e470ec1106f2
Jammonjones <wjssaan@gmail.com>
2023-02-12 03:41:50 -0700

    comment

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -8,3 +8,8 @@ class Program
         test.DisplayScripture();
     }
 }
+
+// To excede requirements I have the user read the scripture at the start 
+// and enter in some sort of hint to help them if needed while memorizing 
+// the user then has a choice while memorizing the scripture to look at it
+// but it doesn't show by default.
